cedd8fcb9a3b57a0965c4853e301e7fbe86ce5fc
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-09-06 15:44:11 +0200

    100%

--- src/main/java/com/epam/rd/autocode/map/Config.java
+++ src/main/java/com/epam/rd/autocode/map/Config.java
@@ -1,7 +1,6 @@
 package com.epam.rd.autocode.map;
 
 import java.io.*;
-import java.util.Enumeration;
 import java.util.Properties;
 
 public class Config {
@@ -22,19 +21,41 @@ public class Config {
             if (defaultFilenames != null) {
                 String[] filenames = defaultFilenames.split(",");
                 for (int i = filenames.length - 1; i >= 0; i--) {
-                    String filename = filenames[1].trim() + ".properties";
+                    String filename = filenames[i].trim() + ".properties";
                     try (InputStream reader = new FileInputStream(filename)) {
-                        Properties defaultPops2 = new Properties();
-                        defaultPops2.load(reader);
-
+                        Properties defaultProp = new Properties();
+                        defaultProp.load(reader);
                     }
                 }
             }
         }
     }
 
-    public String get(String key) {
-        return config.getProperty(key);
+    public String get(String key) throws IOException {
+        try (InputStream input = new FileInputStream("config.properties")) {
+            if (config.isEmpty()) {
+                config.load(input);
+            } else {
+                if (config.getProperty(key) == null) {
+
+                    String defaultFilenames = config.getProperty("default.filenames");
+                    if (defaultFilenames != null) {
+                        String[] filenames = defaultFilenames.split(",");
+                        for (String s : filenames) {
+                            String filename = s.trim() + ".properties";
+                            try (InputStream reader = new FileInputStream(filename)) {
+                                Properties defaultProp = new Properties();
+                                defaultProp.load(reader);
+                                if (defaultProp.getProperty(key) != null) {
+                                    return defaultProp.getProperty(key);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return config.getProperty(key);
+        }
     }
 
     public void remove(String key) {
